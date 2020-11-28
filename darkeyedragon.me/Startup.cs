cc7e99ad8b5e137e4ffcfab1d39d192bbe668699
DarkEyeDragon <joshua.tack@student.vives.be>
2020-11-28 15:12:01 +0100

    Add api project

--- darkeyedragon.me/Startup.cs
+++ darkeyedragon.me/Startup.cs
@@ -62,16 +62,12 @@ namespace darkeyedragon.me
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                  name: "areas",
-                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                );
             });
         }
     }
